d158c420cc4d632f67f8b71cc8b692fee97f6db0
Abhishek Borse <abhishek.borse10@gmail.com>
2020-01-21 22:56:41 +0530

    add haversine formula

--- location/haversine_formula.py
+++ location/haversine_formula.py
@@ -1,4 +1,7 @@
 from math import sin, cos, pi, atan2, sqrt
+import logging
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
 
 
 def haversine_formula(point1_details, point2_details):
@@ -7,8 +10,8 @@ def haversine_formula(point1_details, point2_details):
     point1_lat: float = float(point1_details.get('lat'))
     point1_long: float = float(point1_details.get('long'))
 
-    point2_lat: float = float(point2_details.get('long'))
-    point2_long: float = float(point2_details.get('lat'))
+    point2_lat: float = float(point2_details.get('lat'))
+    point2_long: float = float(point2_details.get('long'))
 
     lat_diff = point2_lat - point1_lat
     lat_diff = lat_diff * pi / 180
@@ -19,11 +22,11 @@ def haversine_formula(point1_details, point2_details):
         long_diff / 2) * sin(long_diff / 2)
     c = 2 * atan2(sqrt(area), sqrt(1 - area))
     '''distance in km'''
-    distance = c * earth_radius
-    return distance
+    return round(c * earth_radius, 3)
 
 
 if __name__ == '__main__':
-    point1 = {'lat': '', 'long': ''}
-    point2 = {'lat': '', 'long': ''}
-    haversine_formula(point1_details=point1, point2_details=point2)
+    point1 = {'lat': 21.146633, 'long': 79.088860}
+    point2 = {'lat': 18.516726, 'long': 73.856255}
+    distance = haversine_formula(point1_details=point1, point2_details=point2)
+    logger.info('distance is {} km'.format(str(distance)))
